126c25dbce95376a547c26060f6826a5d1a62577
Pili Sánchez <sanchezpili6@gmail.com>
2022-07-15 21:12:51 -0500

    Get current known correct letters

--- wordl_solver.py
+++ wordl_solver.py
@@ -1,4 +1,5 @@
-abecedary = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+abecedary = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+             'V', 'W', 'X', 'Y', 'Z']
 
 '''print("Welcome to the word solver!")
 print("Remember: gray = 0, yellow = 1, green = 2")
@@ -15,19 +16,14 @@ fourth_word = input("Please enter your fourth tried word:").upper()
 fourth_word_result = []
 fourth_word_result = input("Please enter your fourth tried word result separated by commas:").upper()'''
 
-
 first_word = 'crate'
-first_word_result = [0,0,2,0,0]
+first_word_result = [0, 0, 2, 0, 0]
 second_word = 'flunk'
-second_word_result = []
-second_word_result = input("Please enter your second tried word result separated by commas:")
-third_word = input("Please enter your third tried word:").upper()
-third_word_result = []
-third_word_result = input("Please enter your third tried word result separated by commas:")
-fourth_word = input("Please enter your fourth tried word:").upper()
-fourth_word_result = []
-fourth_word_result = input("Please enter your fourth tried word result separated by commas:").upper()
-
+second_word_result = [0, 2, 0, 2, 0]
+third_word = 'plans'
+third_word_result = [0, 2, 2, 2, 0]
+fourth_word = 'clans'
+fourth_word_result = [0, 2, 2, 2, 0]
 
 first_slot_possible_letters = []
 first_slot_definitive_letter = ''
@@ -42,36 +38,41 @@ fifth_slot_definitive_letter = ''
 
 
 def remove_incorrect_letters():
-    for i in first_word_result:
-        int(i)
-        if i == 0:
-            abecedary.remove(first_word[i])
-        if i == 1:
-            add_possible_letter_to_all_but_one_slot(first_word[i], i)
-        if i == 2:
-            add_possible_letter_to_all_but_one_slot(first_word[i], i)
-            add_definitive_letter(first_word[i], i)
-    for i in second_word_result:
-        if i == 0:
-            abecedary.remove(second_word[i])
-        if i == 1:
-            add_possible_letter_to_all_but_one_slot(first_word[i], i)
-        if i == 2:
-            add_possible_letter_to_all_but_one_slot(first_word[i], i)
-    for i in third_word_result:
-        if i == 0:
-            abecedary.remove(third_word[i])
-        if i == 1:
-            add_possible_letter_to_all_but_one_slot(first_word[i], i)
-        if i == 2:
-            add_possible_letter_to_all_but_one_slot(first_word[i], i)
-    for i in fourth_word_result:
-        if i == 0:
-            abecedary.remove(fourth_word[i])
-        if i == 1:
-            add_possible_letter_to_all_but_one_slot(first_word[i], i)
-        if i == 2:
-            add_possible_letter_to_all_but_one_slot(first_word[i], i)
+    for i in range(len(first_word_result)):
+        print(first_word_result[i], first_word[i])
+        if first_word_result[i] == 0:
+            abecedary.pop(i)
+        if first_word_result[i] == 1:
+            add_possible_letter_to_all_but_one_slot(first_word[i], first_word_result[i])
+        if first_word_result[i] == 2:
+            print(add_definitive_letter(first_word[i], i))
+    print('---------')
+    for i in range(len(second_word_result)):
+        print(second_word_result[i], second_word[i])
+        if second_word_result[i] == 0:
+            abecedary.pop(i)
+        if second_word_result[i] == 1:
+            add_possible_letter_to_all_but_one_slot(second_word[i], second_word_result[i])
+        if second_word_result[i] == 2:
+            print(add_definitive_letter(second_word[i], i))
+    print('---------')
+    for i in range(len(third_word_result)):
+        print(third_word_result[i], third_word[i])
+        if third_word_result[i] == 0:
+            abecedary.pop(i)
+        if third_word_result[i] == 1:
+            add_possible_letter_to_all_but_one_slot(third_word[i], third_word_result[i])
+        if third_word_result[i] == 2:
+            print(add_definitive_letter(third_word[i], i))
+    print('---------')
+    for i in range(len(fourth_word_result)):
+        print(fourth_word_result[i], fourth_word[i])
+        if fourth_word_result[i] == 0:
+            abecedary.pop(i)
+        if fourth_word_result[i] == 1:
+            add_possible_letter_to_all_but_one_slot(fourth_word[i], fourth_word_result[i])
+        if fourth_word_result[i] == 2:
+            print(add_definitive_letter(fourth_word[i], i))
     return abecedary
 
 
@@ -123,17 +124,12 @@ def add_possible_letter_to_all_but_one_slot(letter, incorrect_slot):
             fourth_slot_possible_letters.append(letter)
 
 
+correct_letters = ['', '', '', '', '']
+
+
 def add_definitive_letter(letter, correct_slot):
-    if correct_slot == 1:
-        first_slot_definitive_letter = letter
-    if correct_slot == 2:
-        second_slot_definitive_letter = letter
-    if correct_slot == 3:
-        third_slot_definitive_letter = letter
-    if correct_slot == 4:
-        fourth_slot_definitive_letter = letter
-    if correct_slot == 5:
-        fifth_slot_definitive_letter = letter
+    correct_letters[correct_slot] = letter
+    return correct_letters
 
 
 def create_possible_words():
@@ -147,13 +143,11 @@ def create_possible_words():
 
 
 print(remove_incorrect_letters())
-print(first_slot_possible_letters)
-print(second_slot_possible_letters)
-print(third_slot_possible_letters)
-print(fourth_slot_possible_letters)
+print("first slot possible letters: ", first_slot_possible_letters)
+print("second slot possible letters: ", second_slot_possible_letters)
+print("third slot possible letters: ", third_slot_possible_letters)
+print("fourth slot possible letters: ", fourth_slot_possible_letters)
+print("fifth slot possible letters: ", fifth_slot_possible_letters)
+print("first slot definitive letter: ", first_slot_definitive_letter)
+print("correct letters: ", correct_letters)
 print(create_possible_words())
-
-
-
-
-
